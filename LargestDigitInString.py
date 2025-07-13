1e1b4a8919c814ffabcdb9444aca04954df87dee
Abhinav NewBegin <abhinav.newbegin@gmail.com>
2025-07-12 20:55:59 -0700

    day 19

---
+++ LargestDigitInString.py
@@ -0,0 +1,30 @@
+# Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
+
+# An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+# Example 1:
+# Input: s = "dfa12321afd"
+# Output: 2
+# Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
+
+# Example 2:
+# Input: s = "abc1111"
+# Output: -1
+# Explanation: The digits that appear in s are [1]. There is no second largest digit.
+
+def large(s):
+    a = []
+    for i in s:
+        if i.isdigit():
+            num = int(i)
+            if num not in a:
+                a.append(num)
+    if len(a)<2:
+        return -1
+    large = max(a)
+    a.remove(large)
+    second = max(a)
+    return second
+
+s = "dfa12321afd"
+print(large(s))
